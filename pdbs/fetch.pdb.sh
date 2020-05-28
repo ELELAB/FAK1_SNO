686930bb700ae3a33cceec4e8e0b100fa47f10be
Elena Papaleo <elena@kb-bioinfo01.cancer.dk>
2020-05-28 19:30:18 +0200

    pdbs

--- pdbs/fetch.pdb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget -O "$1".pdb 'http://www.pdb.org/pdb/download/downloadFile.do?fileFormat=pdb&compression=NO&structureId='$1;
--- pdbs/pdbredo/readme.txt
+++ pdbs/pdbredo/readme.txt
@@ -1,1 +1,4 @@
 #entries from PDBredo for 2J0K and 2J0J
+can be downloaded from:
+https://pdb-redo.eu/db/2j0j
+https://pdb-redo.eu/db/2j0k 
